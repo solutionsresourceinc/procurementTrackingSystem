32afe43548db3616c437c7a00a440254f512e172
Edu Iglesias <duthesecond@gmail.com>
2014-06-19 10:36:59 +0800

    created purchase request migration

---
+++ app/database/migrations/2014_06_19_022137_create_purchase_request_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseRequestTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('purchase_request', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('projectPurpose', 255);
+			$table->string('sourceOfFund', 255);
+			$table->string('amount', 255);
+			$table->string('office', 255);
+			$table->string('requisitioner', 255);
+			$table->string('modeOfProcurement', 255);
+			$table->string('ControlNo', 255);
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('purchase_request');
+	}
+
+}
