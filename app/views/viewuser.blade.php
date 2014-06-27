a6cdd2b03a1c1210aaaef1fcd74fc361c6b9d965
jjsarmiento <sarmiento11102@gmail.com>
2014-06-27 11:10:26 +0800

    Updated more modals

--- app/views/viewuser.blade.php
+++ app/views/viewuser.blade.php
@@ -34,7 +34,7 @@
         <h4 class="modal-title">Activate User</h4>
       </div>
       <div class="modal-body">
-        <p>Are you sure about this ?</p>
+        <p>Are you sure you want to activate user account?</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
