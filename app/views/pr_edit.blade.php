d1615a9738a1a6d06be5a21c8799e10e1590fc34
Edu Iglesias <duthesecond@gmail.com>
2014-06-30 18:04:27 +0800

    fixed amount field in create purchase request

--- app/views/pr_edit.blade.php
+++ app/views/pr_edit.blade.php
@@ -189,6 +189,7 @@ Attach Image
     <script type="text/javascript">
         function numberWithCommas(x) 
         {
+            x = x.replace(',','');
             x = parseFloat(x).toFixed(2);
             var parts = x.toString().split(".");
             parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
