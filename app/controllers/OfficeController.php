e0c263e299a7372ab0f815eb5dfce78822905e4f
Edu Iglesias <duthesecond@gmail.com>
2014-07-02 14:44:07 +0800

    added ajax functionality in office

--- app/controllers/OfficeController.php
+++ app/controllers/OfficeController.php
@@ -67,17 +67,52 @@ class OfficeController extends BaseController {
 	 */
 	public function update($id)
 	{
-		$rules = ['ofcname' => 'required|alpha_spaces|max:100|allNum'];
-		$validation = Validator::make(Input::all(), $rules);
+		$rules = ['ofcname' => 'required|alpha_spaces|max:100|allNum|unique:offices,officeName'];
+		$messages = array(
+		    'required' => 'The :attribute field is required.',
+		    'unique' => 'The office name has already been taken.',
+		    'alpha_spaces' => 'Office name should contain spaces or alpha characters.',
+		    'max' => 'the maximum lenght for office name is 100',
+		);
+		$validation = Validator::make(Input::all(), $rules,$messages);
 
-		if($validation->fails()){
-			return Redirect::back()->withInput()->withErrors($validation->messages());
+		if($validation->fails())
+		{
+			//return Redirect::back()->withInput()->withErrors($validation->messages());
+			$updateOffice = Office::find($id);
+			$message = $validation->messages()->first();
+			$data = array(
+				"fragments" => array(
+					"#other_message" => "<div class='alert alert-danger' id='other_message'>$message</div>"
+				),
+				"inner-fragments" => array(
+					"#display_$id" =>"<span id='insert_$id' class='current-text mode1'> $updateOffice->officeName  </span>"
+				),
+			);
+
+
+			return Response::json($data);
+		}
+		else
+		{
+			$updateOffice = Office::find($id);
+			$oldOfficeName = $updateOffice->officeName;
+			$updateOffice->officeName = Input::get('ofcname');
+			$updateOffice->save();
+			$data = array(
+				"fragments" => array(
+					"#other_message" => "<div class='alert alert-success' id='other_message'>Changed $oldOfficeName to $updateOffice->officeName </div>"
+				),
+				"inner-fragments" => array(
+					"#display" =>"<span class='current-text mode1'> $updateOffice->officeName  </span>"
+				),
+
+			);
+			return Response::json($data);
 		}
 		
-		$updateOffice = Office::find($id);
-		$updateOffice->officeName = Input::get('ofcname');
-		$updateOffice->save();
-		return Redirect::to('/offices')->with('success','Successfully updated office name');
+		
+		//return Redirect::to('/offices')->with('success','Successfully updated office name');
 	}
 
 	/**
