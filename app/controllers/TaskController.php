f3424b36615f7f6681088fa0870517fcfef585a6
Edu Iglesias <duthesecond@gmail.com>
2014-07-04 15:18:37 +0800

    added due date

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -37,7 +37,20 @@ class TaskController extends Controller {
 		$user_id = Auth::user()->id;
 		$taskDetails = TaskDetails::find($id);
 		$taskDetails->assignee_id = $user_id;
+		$taskDetails->status = "Active";
+		
+		$task_row = Task::find($taskDetails->task_id);
+		$addToDateReceived = $task_row->maxDuration;
+
+		// Get date today and the due date;
+		$dateReceived = date('Y-m-d H:i:s');
+		$dueDate = date('Y-m-d H:i:s', strtotime("$addToDateReceived days"));
+
+		$taskDetails->dateReceived = $dateReceived;
+		$taskDetails->dueDate = $dueDate;
+
 		$taskDetails->save();
+
 		return Redirect::to('task/task-id');
 	}
 
