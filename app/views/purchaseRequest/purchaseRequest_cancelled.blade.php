f732b22b674d87bce3aad58f4a3c5bdf3036d613
johnarben2468 <johnarbennicolehombrebueno@yahoo.com.ph>
2014-07-11 11:02:38 +0800

    Modified PR listings

--- app/views/purchaseRequest/purchaseRequest_cancelled.blade.php
+++ app/views/purchaseRequest/purchaseRequest_cancelled.blade.php
@@ -1,26 +1,43 @@
 @extends('layouts.dashboard')
 
-
 @section('content')
-    <h1 class="page-header">List of Cancelled Purchase Requests</h1>
+
+
+<h1 class="pull-left">List of Active Purchase Requests</h1>
+    
+    @if ( Entrust::hasRole('Administrator') || Entrust::hasRole('Procurement Personnel'))
+      <div class="pull-right options">
+          <a href="{{ URL::to('purchaseRequest/create') }}" class="btn btn-success">Create New</a>
+      </div>
+    @endif
+
+    <hr class="clear" />
+<div id="pr_form">
+    <form action="submitForm/" id="new_form" method="post" id="confirm">
+</div>
 
     <div class="modal fade" id="confirmDelete" role="dialog" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
+                
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Delete Purchase Request</h4>
+                    <h4 class="modal-title">Cancel Purchase Request</h4>
                 </div>
                 <div class="modal-body">
-                    <p>Are you sure you want to delete purchase request?</p>
+                    <p>Reason for cancelling purchase request:</p>
+                    <textarea id="reason" class="form-control" rows="3" maxlength="255", style="resize:vertical"></textarea>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-danger" id="confirm">Delete</button>
+                    <button type="button" class="btn btn-success" onClick="submitForm()">Cancel Purchase Request</button>
+                    
                 </div>
+            
             </div>
         </div>
     </div>
+</form>
 
     <div class="modal fade" id="confirmActivate" role="dialog" aria-labelledby="confirmActivateLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -39,92 +56,101 @@
             </div>
         </div>
     </div>
-
-    <table id="table_id" class="table table-striped display">
+@if(Session::get('notice'))
+            <div class="alert alert-success"> {{ Session::get('notice') }}</div> 
+            @endif
+    <table id="table_id" class="table table-striped display ">
         <thead>
-    		<tr>
-    	    	<th>Control No.</th>
+            <tr>
+                <th>Control No.</th>
                 <th>Project/Purpose</th>
                 <th>Mode</th>
-    	      	<th style="text-align: center">Status</th>
-    	      	<th>Date Requested</th>
-                <?php
-                    $adm = Assigned::where('user_id', Auth::User()->id)->first();
-                    //if($adm->role_id == 3) {
-                ?>
-
-                <?php //} ?>
-        	</tr>
+                <th style="text-align: center">Status</th>
+                <th>Date Requested</th>
+               
+                
+              </tr>
         </thead>
- <?php  //Restrictions
-  
-            $requests = new Purchase;
-            $reqrestrict=0;
-            $userx=Auth::user()->id;
-           if  (Entrust::hasRole('Administrator'))
-            $requests = DB::table('purchase_request')->where('status', '=', 'Cancelled')->get(); //change this to get overdue PRs
-          else if  (Entrust::hasRole('Procurement Personel'))
-            $requests = DB::table('purchase_request')->where('status', '=', 'Cancelled')->where('created_by', $userx)->get();
-            else 
-            { 
 
-                $requests = DB::table('purchase_request')->where('status', '=', 'Cancelled')->get();
-                $reqrestrict=1;
-            }
-            //End Restrictions
-      ?>
-      	<tbody>
+        <?php
+           //Query Restrictions
+            $date_today =date('Y-m-d H:i:s');
+            $requests = new Purchase;
+           
+            $userx=Auth::user()->id;
+              $requests = DB::table('purchase_request')->where('status', '=', 'Cancelled')->get(); 
+            //End Query Restrictions
+        ?>
+
+        <tbody>
             @if(count($requests))
                 @foreach ($requests as $request)
+
                     <tr
- <?php 
- //Office restriction for requis
-                    if($reqrestrict==1)
+                      <?php 
+                     //Office restriction
+                    if (Entrust::hasRole('Administrator'))
+                      {}
+                    else if(Entrust::hasRole('Procurement Personnel')){
+                            
+                            $useroffice=Auth::user()->office_id;
+                            $maker= User::find( $request->requisitioner);
+                             $docget=Document::where('pr_id', $request->id)->first();
+                        $taskd = TaskDetails::where('doc_id',$docget->id)->where('assignee_id',$userx)->count();
+                            if($taskd!=0){}
+                            else if ($userx==$request->created_by)
+                            {}
+                            else if ($useroffice!=$maker->office_id)
+                                continue;
+                    }
+                    else
                         {
                             $useroffice=Auth::user()->office_id;
                             $maker= User::find( $request->requisitioner);
                             if ($useroffice!=$maker->office_id)
                                 continue;
                         }
-//End office restriction for requis
+                //End Office restriction
                         $doc = new Document; $doc = DB::table('document')->where('pr_id', $request->id)->first();  
                         $doc_id= $doc->id;
                     $userx= Auth::User()->id;
+                    $counter=0;
                     $counter=Count::where('user_id', $userx)->where('doc_id', $doc_id)->count();
                     if ($counter!=0){
                         echo "class ='success'";
                     }
 
                     ?>
-
-                    >
+                        >
                         <td width="10%">{{ $request->controlNo; }}</td>
                         <td width="30%"><a data-toggle="tooltip" data-placement="top" class="purpose" href="{{ URL::to('purchaseRequest/vieweach/'. $request->id) }}" title="View Project Details">{{ $request->projectPurpose; }}</a></td>
-                        <?php $doc = new Document; $doc = DB::table('document')->where('pr_id', $request->id)->get(); ?>
+                        <?php 
+                            $doc = new Purchase; 
+                            $doc = DB::table('document')->where('pr_id', $request->id)->get(); 
+                        ?>
                         <td width="18%">
                             @foreach ($doc as $docs) {{ Workflow::find($docs->work_id)->workFlowName; }} @endforeach
                         </td>
-                        <td width="12%" style="text-align: center"><span class="label label-default">{{ $request->status; }}</span></td>
+                        <td width="12%" style="text-align: center"><span class="label {{($request->status == 'New') ? 'label-primary' : (($request->status == 'Active') ? 'label-success' : (($request->status == 'Overdue') ? 'label-danger' : 'label-default'))}}">{{ $request->status; }}</span></td>
                         <td width="20%">{{ $request->dateRequested; }}</td>
-                        <?php
-                            if($adm->role_id == 3) {
-                        ?>
 
-                       <?php } ?>
+                       
                    </tr>
-               @endforeach	 
+                @endforeach
             @else
                 <tr>
                     <td colspan="<?php if($adm->role_id == 3) echo "6"; else echo "5";?>">
                         <span class="error-view">No data available.</span>
                     </td>
                 </tr>
-            @endif   
-      	</tbody>
+            @endif
+        </tbody>
     </table>  
+                {{ Session::forget('notice'); }}
 @stop
 
 @section('footer')
+    {{ HTML::script('js/bootstrap-ajax.js');}}
     <script type="text/javascript">
         $('#confirmDelete').on('show.bs.modal', function (e) {
             $message = $(e.relatedTarget).attr('data-message');
@@ -149,5 +175,26 @@
             window.my_id = pass_id;
            // alert(window.my_id);
         }
+
+        function submitForm()
+        {
+            var reason = document.getElementById('reason').value;
+            document.getElementById('hide_reason').value = reason;
+            //alert(reason);
+            document.getElementById("form").submit();
+        }
+        
     </script>
+
+{{ Session::forget('main_error'); }}
+{{ Session::forget('m1'); }}
+{{ Session::forget('m2'); }}
+{{ Session::forget('m3'); }}
+{{ Session::forget('m4'); }}
+{{ Session::forget('m5'); }}
+{{ Session::forget('m6'); }}
+{{ Session::forget('m7'); }}
+{{ Session::forget('imgsuccess'); }}
+{{ Session::forget('imgerror'); }}
+
 @stop
