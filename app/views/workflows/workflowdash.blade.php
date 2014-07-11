89c02b248dd1fd71615877ea210196ce2b6b6e31
mgeronimo <martha@solutionsresource.com>
2014-07-11 11:20:57 +0800

    updated workflow dashboard to keep current tab open when reloaded

--- app/views/workflows/workflowdash.blade.php
+++ app/views/workflows/workflowdash.blade.php
@@ -66,7 +66,21 @@
                 <script>
                   $(function () {
                     $('#myTab a:first').tab('show')
-                  })
+                  });
+                  $(function() { 
+                      //for bootstrap 3 use 'shown.bs.tab' instead of 'shown' in the next line
+                      $('a[data-toggle="tab"]').on('click', function (e) {
+                        //save the latest tab; use cookies if you like 'em better:
+                        localStorage.setItem('lastTab', $(e.target).attr('href'));
+                      });
+
+                      //go to the latest tab, if it exists:
+                      var lastTab = localStorage.getItem('lastTab');
+
+                      if (lastTab) {
+                          $('a[href="'+lastTab+'"]').click();
+                      }
+                    });
                 </script>
                 <script>
                 function empty_div()
