fbce5adeeb27739f35baa7c2edde55ef5addb715
Edu Iglesias <duthesecond@gmail.com>
2014-08-11 13:19:25 +0800

    change totalDays on other workflows

--- app/database/seeds/WorkflowSeeder.php
+++ app/database/seeds/WorkflowSeeder.php
@@ -14,25 +14,25 @@ class WorkflowSeeder extends Seeder {
         $flow = new Workflow;
         $flow->id = '2';
         $flow->workFlowName = 'Small Value Procurement (Above P50,000 Below P500,000)';
-        $flow->totalDays = '45';
+        $flow->totalDays = '120';
         $flow->save();
 
         $flow = new Workflow;
         $flow->id = '3';
         $flow->workFlowName = 'Bidding (Above P500,000)';
-        $flow->totalDays = '45';
+        $flow->totalDays = '200';
         $flow->save();
         
         $flow = new Workflow;
         $flow->id = '4';
         $flow->workFlowName = 'Pakyaw';
-        $flow->totalDays = '45';
+        $flow->totalDays = '200';
         $flow->save();
 
         $flow = new Workflow;
         $flow->id = '5';
         $flow->workFlowName = 'Direct Contracting';
-        $flow->totalDays = '45';
+        $flow->totalDays = '200';
         $flow->save();
     }
 }
