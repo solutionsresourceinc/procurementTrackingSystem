829526c5502c2578bec5d6a528c1176d3d509b08
jjsarmiento <sarmiento11102@gmail.com>
2014-08-28 13:18:34 +0800

    Solid border for complete Table view

--- app/views/purchaseRequest/purchaseRequest_completeTable.blade.php
+++ app/views/purchaseRequest/purchaseRequest_completeTable.blade.php
@@ -138,13 +138,13 @@
     </form>
 
 <div style="margin-top: 30px">
-    <table class="table table-striped display" id="main-table" border="1">
+    <table class="" id="main-table" border="1" style="border-style: solid; border-color: black;">
         <thead>
             <th colspan="11"> <b> {{{ $pageType }}} </b> </th>
         </thead>
         <thead>
-            <th width="9.09%"> DATE </th>
-            <th width="9.09%"> BAC NO </th>
+            <th width="7.09%"> DATE </th>
+            <th width="5.09%"> BAC NO </th>
             <th width="9.09%"> DEPARTMENT </th>
             <th width="9.09%"> PARTICULARS </th>
             <th width="9.09%"> BUDGET </th>
@@ -153,7 +153,7 @@
             <th width="9.09%"> DATE APPROVED </th>
             <th width="9.09%"> MOP </th>
             <th width="9.09%"> SUPPLIER </th>
-            <th width="9.09%"> REMARKS </th>
+            <th width="15.09%"> REMARKS </th>
         </thead>
         <tbody>
             @foreach($requests as $request)
